71696d5c13e9a9418f512ac723ff1cb375d7a818
Alexey Kulikov <a.kulikov@gmail.com>
2014-01-14 17:08:44 +0100

    Added selected text to the contextual menu, and fixed a bug in frame handlers

--- Injected.js
+++ Injected.js
@@ -1,19 +1,36 @@
-function handleMessage(msg) {
-    var sel = window.getSelection()+'';
-    switch(msg.name){
-        case 'getSelectedTextForP4TTicket': {
-            safari.self.tab.dispatchMessage('theSelectionForP4TTicket', sel);
-            break;
-        }
+    /**
+     *  @author Alexey Kulikov <clops>
+     **/
+     
+    function handleMessage(msg) {
+        var sel = getSelectedText();
         
-        case 'getSelectedTextForAgileZENCard': {
-            safari.self.tab.dispatchMessage('theSelectionForAgileZenCard', sel);
-            break;  
+        //push the selection to the global scope
+        safari.self.tab.setContextMenuEventUserInfo(sel);
+        
+        switch(msg.name){
+            case 'getSelectedTextForP4TTicket': {
+                safari.self.tab.dispatchMessage('theSelectionForP4TTicket', sel);
+                break;
+            }
+        
+            case 'getSelectedTextForAgileZENCard': {
+                safari.self.tab.dispatchMessage('theSelectionForAgileZenCard', sel);
+                break;  
+            }            
         }
     }
-}
+    
+    function handleSelectedText(msg) {
+        var sel = getSelectedText();
+        safari.self.tab.setContextMenuEventUserInfo(msg, sel);
+    }
+    
+    function getSelectedText() {
+        var sel = window.getSelection()+'';
+        sel.replace(/^\s+|\s+$/g,""); //trim
+        return sel;
+    }
 
-if (window.top === window) {
     safari.self.addEventListener("message", handleMessage, false);
-}
-
+    document.addEventListener("contextmenu", handleSelectedText, false);
